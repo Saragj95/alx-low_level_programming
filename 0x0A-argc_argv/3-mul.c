aa09bc8b8bfb435e68bca6af612fed4f7616fc6d
Saragj95 <sara.guerrouj.22@gmail.com>
2023-10-27 19:45:13 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,6 +3,20 @@
 #include "main.h"
 
 /**
+ * isInteger - Check if the input string represents an integer.
+ * @str: The string to check.
+ *
+ * Return: 1 if the string is a valid integer, 0 otherwise.
+ */
+
+int isInteger(const char *str)
+{
+	int num = atoi(str);
+
+	return (num != 0 || (num == 0 && str[0] == '0'));
+}
+
+/**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -19,12 +33,17 @@ int main(int argc, char *argv[])
 		printf("ERROR\n");
 		return (1);
 	}
-	else
+	if (isInteger(argv[1]) && isInteger(argv[2]))
 	{
 		val1 = atoi(argv[1]);
 		val2 = atoi(argv[2]);
 		mul = val1 * val2;
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("ERROR\n");
+		return (1);
 	}
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int main(int argc, char *argv[]);
+int isInteger(const char *str);
 
 #endif
