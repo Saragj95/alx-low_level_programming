5b71c38661e347cde46cd8ba68624ee39199e6d3
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-02 22:44:30 +0000

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,19 +20,28 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	matrix = (int **)malloc(width * sizeof(int *));
-	for (i = 0; i < width; i++)
-	{
-		matrix[i] = (int *)malloc(height * sizeof(int));
-	}
 	if (matrix == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < width; i++)
 	{
+		matrix[i] = (int *)malloc(height * sizeof(int));
+		if (matrix[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(matrix[j]);
+			}
+			free(matrix);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < width; i++)
+	{
 		for (j = 0; j < height; j++)
 		{
-			matrix[i][j] = 0 + 0;
+			matrix[i][j] = 0;
 		}
 	}
 	return (matrix);
