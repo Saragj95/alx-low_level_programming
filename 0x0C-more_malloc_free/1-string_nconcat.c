b8e0f1f5ad00abb4649c3e307ad844dccac58eb2
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-15 13:16:10 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,11 +18,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 	len = strlen(s1) + n;
 	string = malloc(sizeof(char) * len);
