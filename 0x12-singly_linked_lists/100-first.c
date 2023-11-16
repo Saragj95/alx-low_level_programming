13ea65049ba480575baeb9393078a0507574913d
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-16 13:49:46 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,13 @@
 #include "lists.h"
 
 /**
- * prints - prints a sentence before the main function is executed
+ * prints - prints a sentence
+ * before the main function
  */
 
 void prints(void) __attribute__((constructor));
 void prints(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
