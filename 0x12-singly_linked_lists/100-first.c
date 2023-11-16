76e429db5ae810382644defe262f45d7c950fa4c
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-16 13:53:26 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+void prints(void) __attribute__((constructor));
+
 /**
  * prints - prints a sentence
  * before the main function
  */
 
-void prints(void) __attribute__((constructor));
 void prints(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
