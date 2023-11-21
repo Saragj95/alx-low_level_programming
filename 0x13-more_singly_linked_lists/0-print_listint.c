fdf7d21a87510788985c55c41a128d6784c7f5c7
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-21 11:52:46 -0800

    commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr;
+	int i = 0;
+
+	ptr = malloc(sizeof(listint_t));
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
