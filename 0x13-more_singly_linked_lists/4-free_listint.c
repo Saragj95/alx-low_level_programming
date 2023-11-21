94098694cc8e38475af30d09ad2ecc26f485389c
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-21 14:58:18 -0800

    commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,15 +9,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	if (head == NULL)
-	{
-		return;
-	}
 	while (head != NULL)
 	{
 		ptr = head->next;
 		free(head);
 		head = ptr;
 	}
-	free(ptr);
 }
