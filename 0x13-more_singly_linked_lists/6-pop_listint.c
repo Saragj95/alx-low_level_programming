1288315f4ef417983250cf09816df7e1b1e97211
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-30 07:59:59 +0100

    commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,12 +3,15 @@
 int pop_listint(listint_t **head)
 {
 	int i;
+	listint_t *temp;
 
 	if (*head == NULL)
 	{
 		return(0);
 	}
-	i = head->n;
-	free(head);
+	i = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	(*head) = temp;
 	return(i);
 }
