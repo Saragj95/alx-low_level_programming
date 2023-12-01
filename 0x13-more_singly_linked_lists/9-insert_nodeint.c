2eab2b7c921a251f3d172f7c1bc6bbae15c9ed03
Saragj95 <sara.guerrouj.22@gmail.com>
2023-12-01 12:49:41 +0100

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,10 +22,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->n = n;
 	newnode->next = NULL;
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		newnode->next = (*head);
